8f713889462f54070aa677441b316c0a34778364
Dima <poipoilion@gmail.com>
2022-10-05 14:17:05 +0700

    Create project

---
+++ src/main/java/com/university/My/profession/MyProfessionApplication.java
@@ -0,0 +1,13 @@
+package com.university.My.profession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProfessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProfessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/university/My/profession/MyProfessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.My.profession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProfessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
